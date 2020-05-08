4e8ee238e7d847e8dde5c83019b409e63c1d88d0
Victoria Ukatu <victoria.ukatu@gmail.com>
2020-05-08 17:02:36 -0500

    Initial commit with React setup and basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +6,20 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 class="main-title">#stayhomehavefun #quarantinelife</h1>
         </header>
+        <h2 class="intro-title">I'm not sure about you, but sometimes stay at home life has me feeling like this:</h2>
+        <div>
+          <img src="https://rockysretreat.com/rrmedia/dog-sleeping-on-couch-web-sized.jpg" alt="dog sleeping on couch" />
+        </div>
+        <h3 class="intro-title">Instead of snoozing all day, check out some of these activities to entertain yourself * IN PROGRESS *</h3>
+        <div class="button-holder">
+          <span class="emoji" id="emoji-face" role="img" aria-label="happy emoji">😆</span>
+          <button class="button">PAIRING GAME</button>
+          <button class="button">MAZE GAME</button>
+          <button class="button">GUESSING GAME</button>
+          <span class="emoji" role="img" aria-label="raised hands emoji">🙌🏽</span>
+        </div>
       </div>
     );
   }
