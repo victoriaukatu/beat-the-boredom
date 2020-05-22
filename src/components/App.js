255809d9180d7d75c44e497e51e09cda84bcdd2c
Victoria Ukatu <victoria.ukatu@gmail.com>
2020-05-22 17:36:14 -0500

    Add Link to navigate to pairing game page

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { 
-  Route, 
+import {
+  Route,
   HashRouter as Router,
+  Link,
   // Redirect,
   Switch
- } from 'react-router-dom';
+} from 'react-router-dom';
 import './App.css';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import PairingGame from './PairingGame';
 
 class App extends Component {
@@ -16,29 +17,29 @@ class App extends Component {
         <header className="App-header">
           <h1 class="main-title">#stayhomehavefun #quarantinelife</h1>
         </header>
-        <h2 class="intro-title">I'm not sure about you, but sometimes stay at home life has me feeling like this:</h2>
+        <h2 class="intro-title">I'm not sure about you, but sometimes stay-at-home life has me feeling like this:</h2>
         <div>
           <img src="https://rockysretreat.com/rrmedia/dog-sleeping-on-couch-web-sized.jpg" alt="dog sleeping on couch" />
         </div>
         <h3 class="intro-title">Instead of snoozing all day, check out some of these activities to entertain yourself * IN PROGRESS *</h3>
-        <div class="button-holder">
-          <span class="emoji" id="emoji-face" role="img" aria-label="happy emoji">😆</span>
-          <button class="button">PAIRING GAME</button>
-          <button class="button">MAZE GAME</button>
-          <button class="button">GUESSING GAME</button>
-          <span class="emoji" role="img" aria-label="raised hands emoji">🙌🏽</span>
-          <Router>
+        <Router>
+          <div class="button-holder">
+            <span class="emoji" id="emoji-face" role="img" aria-label="happy emoji">😆</span>
+            <Link to="/pairing-game"><button class="button">PAIRING GAME</button></Link>
+            <button class="button">MAZE GAME</button>
+            <button class="button">GUESSING GAME</button>
+            <span class="emoji" role="img" aria-label="raised hands emoji">🙌🏽</span>
             <Switch>
-            <Route 
-              exact path="/pairing-game"
-              component={PairingGame}
-            />
+              <Route
+                exact path="/pairing-game"
+                component={PairingGame}
+              />
             </Switch>
-          </Router>
-        </div>
+          </div>
+        </Router>
       </div>
     );
   }
 }
 
-export default connect() (App);
+export default (App);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
+// import { createStore } from 'redux';
 import App from './components/App';
 
-const store = createStore(
-  );
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('react-root'),
+    <App />,
+  document.getElementById('root'),
 );
 
